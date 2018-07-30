369159de11d79cf45be3cb49f3e6c34e33b0f1ed
Carla Diví Cuesta <carla.divi@est.fib.upc.edu>
2018-07-30 13:08:06 +0200

    Waffle people

--- frontend/js/wafflevf.js
+++ frontend/js/wafflevf.js
@@ -1,5 +1,6 @@
 
   var dataw1;
+  var dataw2;
   var parseDate = d3.timeParse("%Y");
   var total;
   var family = ["A","B","C","D","E","F","G","H","I"];
@@ -25,8 +26,7 @@
     return dataw1;
   };
 
-
-  var waffleDraw = function() {
+    var waffleDraw = function() {
     var color = d3.scale.category20();
           //var domain = ["Total", "Làctics i derivats", "Farines i derivats","Arròs,pasta,sucre,llegums", "Conserves i plats preparats", "Fruites i verdures fresques","Olis i greixos",  "Carn,peix,embotits", "Begudes i infusions", "Congelats"];
     var domain = ["Total", "A", "B","Arròs,pasta,sucre,llegums", "Conserves i plats preparats", "Fruites i verdures fresques","Olis i greixos",  "Carn,peix,embotits", "Begudes i infusions", "Congelats"];
@@ -50,10 +50,73 @@
       .call(chart4); 
   };
 
+var waffle2Ajax = function(name,year1,year2) {
+    $(function () {
+        $.ajax({                                      
+          url: name+'.php',                  //the script to call to get data          
+          data: "",
+          //Cambiar a type: POST si necesario
+          type: "GET",
+          // Formato de datos que se espera en la respuesta
+          dataType: 'json',                //data format      
+          success: function(data)          //on recieve of reply
+          {
+            dataw2 = data;
 
-waffleAjax("waffle2",2000,2017,family);
+            } 
+        });
+    });
+    return dataw2;
+  };
+
+
+  var waffle2Draw = function() {
+    var color = d3.scale.category20();
+          //var domain = ["Total", "Làctics i derivats", "Farines i derivats","Arròs,pasta,sucre,llegums", "Conserves i plats preparats", "Fruites i verdures fresques","Olis i greixos",  "Carn,peix,embotits", "Begudes i infusions", "Congelats"];
+    var domain = ["Total", "A", "B","Arròs,pasta,sucre,llegums", "Conserves i plats preparats", "Fruites i verdures fresques","Olis i greixos",  "Carn,peix,embotits", "Begudes i infusions", "Congelats"];
+    
+
+
+    var domain = dataw2.map(function(el) { 
+      return {
+          name: "Persones",
+          value: el.quantitat/10000,
+        };
+      });
+    console.log("waffle perso",domain);
+    var range = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6","#b82e2e", "#dd4477", "#66aa00", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+          //var palette = d3.scale.ordinal().domain(domain).range(range);
+    var palette =  d3.scale.ordinal().domain(domain).range(color);
+    var chart4 = d3waffle()
+                    .rows(2)
+                    .scale(1/4)
+                    .icon("&#xf183;")
+                    //.icon("&#xf1b9;")
+                    .adjust(0.425)
+                    .colorscale(d3.scale.category10())
+                    .appearancetimes(function(d, i){
+                      mod = 13;
+                      val = i % mod;
+                      return val / mod * 150;
+                    })
+                    .height(200);
+    d3.select("#chartwf-2")
+      .datum(domain)
+      .call(chart4); 
+  };
+
+
+waffleAjax("waffle2",2000,2017);
 
 
 setTimeout(function(){
     waffleDraw();
 },500);
+
+waffle2Ajax("wafflePersones",2000,2017);
+
+
+setTimeout(function(){
+    waffle2Draw();
+},500);
+
---
+++ frontend/wafflePersones.php
@@ -0,0 +1,30 @@
+<?php 
+
+include 'db_connection.php';
+
+header('Content-type: application/json');
+
+$conn = OpenCon();
+
+
+$sql = "SELECT SUM(quantitat) AS quantitat FROM beneficiaris_comarca";
+
+  //--------------------------------------------------------------------------
+  // 2) Query database for data
+  //--------------------------------------------------------------------------
+  $result = mysqli_query($conn, $sql);     
+      //query
+  $data = array();
+	while ( $row = mysqli_fetch_assoc($result) )
+	{
+	  $data[] = $row;
+
+	}
+  //--------------------------------------------------------------------------
+  // 3) echo result as json 
+  //--------------------------------------------------------------------------
+  if(!json_encode($data)) var_dump("Error");
+  echo json_encode($data);
+
+
+?>
